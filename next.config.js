412050cd119aa9d8ae2924f956d0985c8c38eaad
Eikhinson <150049676+Eikhinson@users.noreply.github.com>
2025-07-03 22:06:10 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,14 +5,7 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['node-telegram-bot-api']
-  },
-  images: {
-    domains: ['api.a4f.co', 'cdn.openai.com'],
-    unoptimized: true
   }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
