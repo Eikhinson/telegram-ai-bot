ce0018fac50a04e985ff12b1861608309d936cdb
Eikhinson <150049676+Eikhinson@users.noreply.github.com>
2025-07-03 22:14:33 +0300

    Create page.js

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding: '32px'}}>
+      <h1>Telegram AI Bot</h1>
+      <p>Бот @NeuromaniaGPT_bot работает!</p>
+    </main>
+  );
+}
